8c5e407389afa9f0d8c0adc8970f8d414245adb8
Jessica Mårtensson <jessica.martensson90@hotmail.com>
2019-05-23 04:37:35 -0400

    hej

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <script type="text/javascript" src="bundle.js"></script>
-    <title>En stack</title>
-  </head>
-  <body>
-    <h1>Här kan vi leka med en stack</h1>
-    Stacken: <span id="top_of_stack">n/a</span><br>
-    <button type="button" name="peek" id="peek">Vad finns överst på stacken?</button>
-    <button type="button" name="pop" id="pop">Poppa stacken!</button>
-    <button type="button" name="push" id="push">Pusha till stacken</button>
-  </body>
-</html>
+import * as stack from './stack.js';
+
+window.onload = function () {
+  console.log("done");
+  var pop = document.getElementById('pop');
+  var push = document.getElementById('push');
+  var peek = document.getElementById('peek');
+  var display = document.getElementById('top_of_stack');
+
+  pop.addEventListener("click", function() {
+    var text = "Tog bort " + stack.pop();
+    alert(text);
+  });
+  push.addEventListener("click", function() {
+    var x = prompt("Vad ska vi lägga på stacken?");
+    stack.push(x);
+    display.innerHTML = x;
+  });
+  peek.addEventListener("click", function() {
+    display.innerHTML = stack.peek();
+  });
+};
